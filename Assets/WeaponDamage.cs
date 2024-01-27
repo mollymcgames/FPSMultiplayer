f5615ebc670dfab4b91cc3214790af53d0f1fdff
Molly <mollymcconaghy@gmail.com>
2024-01-27 22:46:28 +0000

    add weapon

---
+++ Assets/WeaponDamage.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using ExitGames.Client.Photon.StructWrapping;
+using UnityEngine;
+using Photon.Pun;
+
+public class WeaponDamage : MonoBehaviour
+{
+    public int damage;
+
+    public float fireRate;
+    private float nextFire;
+
+    public Camera camera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nextFire > 0)
+        {
+            nextFire -= Time.deltaTime;
+        }
+        
+        if (Input.GetButton("Fire1") && nextFire <= 0)
+        {
+            nextFire = 1 / fireRate;
+
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        Ray ray = new Ray(camera.transform.position, camera.transform.forward); 
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray.origin, ray.direction, out hit, 100f))
+        {
+            if(hit.transform.gameObject.GetComponent<Health>() != null)
+            {
+                hit.transform.gameObject.GetComponent<PhotonView>().RPC("TakeDamage", RpcTarget.All, damage);
+            }
+        }
+    }
+}
