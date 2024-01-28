89ca867908b9162266e7bc2c3e641eca9b27106b
Molly <mollymcconaghy@gmail.com>
2024-01-28 21:23:04 +0000

    add health and item

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class Health : MonoBehaviour
 {
     public int health;
+
+    public TextMeshProUGUI healthText;
+
 
     [PunRPC]
 
     public void TakeDamage(int damage)
     {
         health -= damage;
+
+        healthText.text = health.ToString();
+        
         if (health <= 0)
         {
             Destroy(gameObject);
