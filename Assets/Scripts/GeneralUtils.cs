d84e31b109f6f67575a5cbe614883b653645512a
Molly <mollymcconaghy@gmail.com>
2024-05-03 14:44:06 +0100

    hardcode dark player win for now

--- Assets/Scripts/GeneralUtils.cs
+++ Assets/Scripts/GeneralUtils.cs
@@ -2,14 +2,22 @@ using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class GeneralUtils : MonoBehaviour
 {
+
+    // public TextMeshProUGUI roundWonByText;
+    //define the winner string to say "dark" or "light" won the game
+    public string theWinner = "Dark";
     // Start is called before the first frame update
     void Start()
     {
         WakeMouse();
         DisconnectFromGame();
+        // Set the text value of "RoundWonByText" to theWinner
+        UpdateRoundWonByText();    
     }
 
     // Update is called once per frame
@@ -29,5 +37,17 @@ public class GeneralUtils : MonoBehaviour
         {
             PhotonNetwork.Disconnect();
         }        
+    }
+
+    void UpdateRoundWonByText()
+    {
+        TextMeshProUGUI roundWonByText = GameObject.Find("RoundWonByText").GetComponent<TextMeshProUGUI>();
+        roundWonByText.text = theWinner;
+    }
+
+    public void UpdateRoundWonBy(string winner)
+    {
+        theWinner = winner;
+        UpdateRoundWonByText();
     }
 }
